42143381dc68bf7f97eb3825a884b245f1ec1852
ishidao0910 <goo.ishidao@gmail.com>
2022-05-05 01:23:07 +0900

    ユーザーに紐づくツイートの取得

--- show_user.py
+++ show_user.py
@@ -1,10 +1,10 @@
 import configparser
+import json
 from requests_oauthlib import OAuth1Session
 
 
 config_ini = configparser.ConfigParser(interpolation=None)
 config_ini.read('config.ini', encoding='utf-8')
-print(config_ini)
 
 # 取得した各種キーを格納-----------------------------------------------------
 consumer_key=config_ini['DEFAULT']['API_KEY']
@@ -20,4 +20,27 @@ oauth = OAuth1Session(
     resource_owner_secret=access_token_secret,
 )
 
-print(oauth)
+# get tweet
+def tweet_input(user_number):
+    """
+    input : user_number 
+        ユーザー固有のuser_id
+    output : json
+        user_idに紐づくユーザーの直近10個のツイート情報
+        tweet_idとテキストの中身を選択している
+    """
+    tweet_number = oauth.get(
+        "https://api.twitter.com/2/users/"+str(user_number)+"/tweets"
+    )
+    if tweet_number.status_code != 200 :
+        raise Exception(
+            "Request returned an error: {} {}".format(tweet_number.status_code, engagement.text)
+        )
+    print("Response code: {}".format(tweet_number.status_code), "user_number")
+    # Saving the response as JSON
+    json_tweet_number = tweet_number.json()
+    # print(json.dumps(json_tweet_number, indent=4, sort_keys=True))
+    return json.dumps(json_tweet_number, indent=4, sort_keys=True, ensure_ascii=False)
+
+# This is my user_id
+print(tweet_input(1094899950922563585))
