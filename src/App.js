669c50401d53748464cf06232d9b36a452edb3a4
ishidao0910 <goo.ishidao@gmail.com>
2022-05-08 13:50:57 +0900

    コンポーネント化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1> test h1 </h1>
+      <p> test p tag </p>
+    </React.Fragment>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,11 @@ import { StrictMode } from "react";
 import React from "react";
 import ReactDom from "react-dom";
 
-// import App from "./App";
-
-const App = () => {
-  return (
-  <React.Fragment>
-    <h1> test h1 </h1>
-    <p> test p tag </p>
-  </React.Fragment>
-  );
-};
+import App from "./App";
 
 ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
-  , document.getElementById("root")
+  </StrictMode>,
+  document.getElementById("root")
 );
