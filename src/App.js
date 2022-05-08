3b7493ade19d07e732e57f5f61f1e917d251fa70
ishidao0910 <goo.ishidao@gmail.com>
2022-05-08 14:01:34 +0900

    ボタンとcssの書き方追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '18px'
+  }
   return (
     <React.Fragment>
-      <h1> test h1 </h1>
-      <p> test p tag </p>
+      <h1 style={{ color: 'red' }}> test h1 </h1>
+      <p style={contentStyle}> test p tag </p>
+      <button onClick={onClickButton}> ボタン </button>
     </React.Fragment>
   );
 };
