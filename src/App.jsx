ec13b4bb39dd98a99406dd4c0147a7ce17a7a9f1
ishidao0910 <goo.ishidao@gmail.com>
2022-05-08 14:23:55 +0900

    再レンダリングとボタン追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./conponents/ColorfulMessage";
 
 const App = () => {
-    const onClickButton = () => alert();
-    return (
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  return (
     <React.Fragment>
-        <h1 style={{ color: "red" }}> test h1 </h1>
-        <ColorfulMessage color="blue"> お元気ですか </ColorfulMessage>
-        <ColorfulMessage color="pink"> 元気です </ColorfulMessage>
-        <button onClick={onClickButton}> ボタン </button>
+      <h1 style={{ color: "red" }}> test h1 </h1>
+      <ColorfulMessage color="blue"> お元気ですか </ColorfulMessage>
+      <ColorfulMessage color="pink"> 元気です </ColorfulMessage>
+      <button onClick={onClickCountUp}> カウントアップ </button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}> on/off </button>
+      <p> {num} </p>
+      {faceShowFlag && <p>( '_')</p>}
     </React.Fragment>
-    );
+  );
 };
 
 export default App;
