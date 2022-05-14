241a1de1463e548453e3102864b8f902d8a8373a
ishidao0910 <goo.ishidao@gmail.com>
2022-05-15 00:30:40 +0900

    要素追加

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def post():
     # print("user id ", user_id)
 
     ff = "following"    
-    max_results = 15 # 取得したいデータ数
+    max_results = 25 # 取得したいデータ数
     user_fields = ["id", "name", "username","description",]
 
     following_users_id = []
